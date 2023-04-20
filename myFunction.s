9b8363efdef56ce7487a368749514a5b609a98c2
lexain12 <lexain12@gmail.com>
2023-04-20 19:10:29 +0300

    made faster

--- myFunction.s
+++ myFunction.s
@@ -2,30 +2,28 @@ global findElementByValue
 
 findElementByValue:
  	mov    rax,QWORD [rdi]
- 	mov    r8,QWORD [rdi+0x8]
+ 	mov    rdi,QWORD [rdi+0x8]
  	mov    rax,QWORD [rax+0x10]
- 	sub    r8,0x1
- 	je     .end
- 	movq   xmm1,QWORD [rsi]
+ 	sub    rdi,0x1
+
+ 	je      .end
+ 	movdqa xmm1,OWORD [rsi]
  	xor    edx,edx
 
     .loop:
         mov    rax,QWORD [rax+0x10]
+        movdqa xmm0,xmm1
         mov    rcx,QWORD [rax]
-        movq   xmm0,QWORD [rcx]
-        pcmpeqd xmm0,xmm1
+        pcmpeqd xmm0,OWORD [rcx]
         pmovmskb ecx,xmm0
         cmp    ecx,0xffff
-        je     .end
+        je     .end1
+
         add    rdx,0x1
-        cmp    rdx,r8
+        cmp    rdx,rdi
         jne    .loop
-
- 	sub    r8,0x2
- 	cmp    r8,rdx
- 	mov    edx,0x0
- 	cmove  rax,rdx
-
 .end:
+ 	xor    eax,eax
+.end1:
  	ret
 
